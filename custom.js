911fda1c97802943a4d4146de9221117a7e1ead0
Steven Cobb <stevecobb91@gmail.com>
2016-10-16 21:23:14 -0400

    add stanza, playSong function

--- custom.js
+++ custom.js
@@ -9,17 +9,35 @@ $( function() {
 	});
 	
 	cryLoader();
+	playSong();
 	
 } );
 
 function cryLoader() {
 	var i;
-	for (i=001; i <= 5; i++) {
+	for (i=1; i <= 5; i++) {
 		var file_num = '00' + i;
 		file_num = file_num.substr(file_num.length - 3, 3);
 		audio_id = 'cry-' + file_num + '';
 		document.getElementById('cry-wrap').innerHTML += '<audio id='+ audio_id +'><source src="assets/cries/mp3/'+ file_num +'.mp3" type="audio/mpeg"></audio>';
 		document.getElementById('cry-wrap').innerHTML += "<button onclick='document.getElementById(\""+audio_id+"\").play();'>"+ file_num +"</button>";
 	}
+}
+
+function playSong() {
+	$(".play").click(function(){
+		(function myLoop (i) {
+			setTimeout(function () {
+				console.log(i);
+				var file_num = '00' + i;
+				file_num = file_num.substr(file_num.length - 3, 3);
+				audio_id = 'cry-' + file_num + '';
+				document.getElementById(audio_id).play();
+				if (++i <= 5) {
+					myLoop(i);
+				} 
+			}, 1000)
+		} ) (1);
+	});
 }
 
