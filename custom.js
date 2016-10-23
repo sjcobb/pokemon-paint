cb35561bd0cc35a904db5535fb3e934d51a82577
Steven Cobb <stevecobb91@gmail.com>
2016-10-23 14:21:55 -0400

    print sprites inside btn, play all text

--- custom.js
+++ custom.js
@@ -20,7 +20,7 @@ function cryLoader() {
 		file_num = file_num.substr(file_num.length - 3, 3);
 		audio_id = 'cry-' + file_num + '';
 		document.getElementById('cry-wrap').innerHTML += '<audio id='+ audio_id +'><source src="assets/cries/mp3/'+ file_num +'.mp3" type="audio/mpeg"></audio>';
-		document.getElementById('cry-wrap').innerHTML += "<button onclick='document.getElementById(\""+audio_id+"\").play();'>"+ file_num +"</button>";
+		document.getElementById('cry-wrap').innerHTML += "<button onclick='document.getElementById(\""+audio_id+"\").play();'><img src=assets/sprites/"+ i +".png></button>";
 	}
 }
 
