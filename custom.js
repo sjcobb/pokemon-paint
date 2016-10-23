f1cdb87bdc033d1aedd90350615e3048c2351a5f
Steven Cobb <stevecobb91@gmail.com>
2016-10-23 14:41:16 -0400

    basic drag & drop example, spaces

--- custom.js
+++ custom.js
@@ -1,43 +1,56 @@
 $( function() {
-	
-	//create a synth and connect it to the master output (your speakers)
-	//var synth = new Tone.Synth().toMaster();
+    
+    //create a synth and connect it to the master output (your speakers)
+    //var synth = new Tone.Synth().toMaster();
 
-	$('.key').click(function(){
-		//play a middle 'C' for the duration of an 8th note
-		//synth.triggerAttackRelease("C4", "8n");
-	});
-	
-	cryLoader();
-	playSong();
-	
+    $('.key').click(function(){
+        //play a middle 'C' for the duration of an 8th note
+        //synth.triggerAttackRelease("C4", "8n");
+    });
+    
+    cryLoader();
+    playSong();
+    
 } );
 
 function cryLoader() {
-	var i;
-	for (i=1; i <= 5; i++) {
-		var file_num = '00' + i;
-		file_num = file_num.substr(file_num.length - 3, 3);
-		audio_id = 'cry-' + file_num + '';
-		document.getElementById('cry-wrap').innerHTML += '<audio id='+ audio_id +'><source src="assets/cries/mp3/'+ file_num +'.mp3" type="audio/mpeg"></audio>';
-		document.getElementById('cry-wrap').innerHTML += "<button onclick='document.getElementById(\""+audio_id+"\").play();'><img src=assets/sprites/"+ i +".png></button>";
-	}
+    var i;
+    for (i=1; i <= 5; i++) {
+        var file_num = '00' + i;
+        file_num = file_num.substr(file_num.length - 3, 3);
+        audio_id = 'cry-' + file_num + '';
+        document.getElementById('cry-wrap').innerHTML += '<audio id='+ audio_id +'><source src="assets/cries/mp3/'+ file_num +'.mp3" type="audio/mpeg"></audio>';
+        document.getElementById('cry-wrap').innerHTML += "<button onclick='document.getElementById(\""+audio_id+"\").play();'><img src=assets/sprites/"+ i +".png></button>";
+    }
 }
 
 function playSong() {
-	$(".play").click(function(){
-		(function myLoop (i) {
-			setTimeout(function () {
-				console.log(i);
-				var file_num = '00' + i;
-				file_num = file_num.substr(file_num.length - 3, 3);
-				audio_id = 'cry-' + file_num + '';
-				document.getElementById(audio_id).play();
-				if (++i <= 5) {
-					myLoop(i);
-				} 
-			}, 1000)
-		} ) (1);
-	});
+    $(".play").click(function(){
+        (function myLoop (i) {
+            setTimeout(function () {
+                console.log(i);
+                var file_num = '00' + i;
+                file_num = file_num.substr(file_num.length - 3, 3);
+                audio_id = 'cry-' + file_num + '';
+                document.getElementById(audio_id).play();
+                if (++i <= 5) {
+                    myLoop(i);
+                } 
+            }, 1000)
+        } ) (1);
+    });
 }
 
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
