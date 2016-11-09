a23a7e684d51db3e4b8e1e5a0b2cad883165d7da
Steven Cobb <stevecobb91@gmail.com>
2016-11-09 02:46:34 +0000

    staff object example

---
+++ js/staff.js
@@ -0,0 +1,9 @@
+/* http://www.musictheory.net/lessons/10 */
+
+var staff = { 
+    fred: { apples: 2, oranges: 4, bananas: 7, melons: 0 }, 
+    mary: { apples: 0, oranges: 10, bananas: 0, melons: 0 }, 
+    sarah: { apples: 0, oranges: 0, bananas: 0, melons: 5 } 
+}
+
+console.log( staff[ 'fred' ][ 'apples' ] );
