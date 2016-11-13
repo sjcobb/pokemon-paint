ca055626c3c40f7ee2bdcc7fde5250b35907aa5e
Steven Cobb <stevecobb91@gmail.com>
2016-11-13 23:43:52 +0000

    require modules, split js

---
+++ js/cry-shifter.js
@@ -0,0 +1,31 @@
+// For any third party dependencies, like jQuery, place them in the lib folder.
+
+// Configure loading modules from the lib directory,
+// except for 'app' ones, which are in a sibling
+// directory.
+requirejs.config({
+    baseUrl: 'lib',
+    paths: {
+        app: '../app'
+    }
+});
+
+requirejs(['app/main']);
+
+var pitchShift = require("lib/pitchshift/pitchshift.js")
+
+//Create a pitch shifting object
+/*var shifter = require("pitch-shift")(
+    function onData(frame) {
+        //Play / write out frame.  Called whenver data is ready
+    },
+    function onTune(t, pitch) {
+        console.log("Got pitch ", pitch, " at time ", t)
+
+        //This is the amount to scale the sample by
+        return 1.0
+    })
+*/
+
+//Feed some data to the shifter
+//shifter(new Float32Array([1, 1, 0, 1, 0, 0, 0 /* ... etc */ ]));
