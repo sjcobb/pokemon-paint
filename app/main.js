ea2afea73166e96289637f6d6538e25874b79f71
Steven Cobb <stevecobb91@gmail.com>
2016-11-14 00:01:56 +0000

    move pitchshift define to main.js

--- app/main.js
+++ app/main.js
@@ -9,4 +9,21 @@ define(function (require) {
     var print = require('print');
 
     print(messages.getHello());
+
+    /*** CRY SHIFT ***/
+    //Create a pitch shifting object
+    var shifter = require("../pitchshift.js")(
+        function onData(frame) {
+        //Play / write out frame.  Called whenver data is ready
+        },
+        function onTune(t, pitch) {
+
+        console.log("Got pitch ", pitch, " at time ", t)
+
+        //This is the amount to scale the sample by
+        return 1.0
+        })
+
+    //Feed some data to the shifter
+    shifter(new Float32Array([1, 1, 0, 1, 0, 0, 0 /* ... etc */ ]))
 });
