42952253d4be75036744382daa402753b93fd51a
Steven Cobb <stevecobb91@gmail.com>
2016-12-12 03:20:46 +0000

    add null condition to staff func

--- js/staff.js
+++ js/staff.js
@@ -13,22 +13,24 @@ function generateStaff() {
 function playSong() {
 	var table = document.querySelector("table");
 	var data  = parseTable(table);
-	console.log(data);
+	//console.log(data);
 
 	var q = 0;
 	var i = 0;
 	(function myLoop (i) {
 		for (q = 0; q < 5; q++) {
 			var temp = data[i][q];
-			console.log(temp);
-			var snd1  = new Audio();
-			var src1  = document.createElement("source");
-			src1.type = "audio/mpeg";
-			src1.src  = "assets/cries/mp3/"+ temp +".mp3";
-			snd1.appendChild(src1);
-			snd1.play();
-			console.log("i = "+ i );
-			console.log("q = "+ q );
+			//console.log("temp = "+ temp);
+			//console.log("i = "+ i );
+			//console.log("q = "+ q );
+			if (temp != null) {
+				var snd1  = new Audio();
+				var src1  = document.createElement("source");
+				src1.type = "audio/mpeg";
+				src1.src  = "assets/cries/mp3/"+ temp +".mp3";
+				snd1.appendChild(src1);
+				snd1.play();
+			}
 		}
 
 		setTimeout(function () {
